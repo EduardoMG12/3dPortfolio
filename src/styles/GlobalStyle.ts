845c351bcbaf641ecc9cf5f2383e644691dd259e
Eduardo <eduardo@pop-os.localdomain>
2023-03-13 16:49:38 -0300

    refactor(): changed styles

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -6,6 +6,7 @@ const GlobalStyle = createGlobalStyle`
         padding: 0;
         box-sizing: border-box;
         margin: 0;
+        list-style: none;
     }
     html{
         font-size: 62.5%;

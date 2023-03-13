c65c45d700481b104591bdaa2b1e481cf8834596
Eduardo <eduardo@pop-os.localdomain>
2023-03-13 19:06:53 -0300

    feat(): created component who

---
+++ src/components/Who/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height:100vh;
+  scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 140rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ContentLeft = styled.div`
+  flex: 1;
+`;

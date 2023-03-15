3bb3f393061fa1b099874e7fd8770e2c4f2eeb39
Eduardo <eduardo@pop-os.localdomain>
2023-03-15 19:09:26 -0300

    refactor(app/page/home): re-structured project

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,18 @@
-import Contact from "@components/Contact"
-import Hero from "@components/Hero"
-import Who from "@components/Who"
-import Works from "@components/Works"
-import styled from "styled-components"
-import background from '/public/images/background.jpeg'
-import GlobalStyle from "@styles/GlobalStyle"
-
-export const Container = styled.div`
-  height:100vh;
-  background-color: rebeccapurple;
-  scroll-snap-type: y mandatory;
-  scroll-behavior: smooth;
-  overflow-y: auto;
-  scrollbar-width: none;
-  color:white;
-  background: url(${background});
-  &::-webkit-scrollbar{
-    display: none;
-  }
-`;
+import Contact from "@components/Contact";
+import Hero from "@components/Hero";
+import Who from "@components/Who";
+import Works from "@components/Works";
+import styled from "styled-components";
+import GlobalStyle from "@styles/GlobalStyle";
+import Home from "./pages/Home";
 
 const App: React.FC = () => {
-
   return (
     <>
       <GlobalStyle />
-      <Container>
-        <Hero />
-        <Who />
-        <Works />
-        <Contact />
-      </Container>
+      <Home />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;

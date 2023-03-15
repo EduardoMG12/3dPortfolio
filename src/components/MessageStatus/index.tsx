7f9738dd44ba68a537259d331dbb00086c8e3889
Eduardo <eduardo@pop-os.localdomain>
2023-03-15 20:53:40 -0300

    feat(message/form): added message for validation

--- src/components/MessageStatus/index.tsx
+++ src/components/MessageStatus/index.tsx
@@ -1,29 +1,36 @@
-import React from 'react';
-import { P } from './styles';
+import React from "react";
+import { P } from "./styles";
 
 export type PropsMessage = {
   text?: string;
   messageError?: string;
   messageSuccess?: string;
   isSuccess: boolean | null;
-  isValidated: boolean | null
+  isValidated: boolean | null;
 };
 
-const MessageStatus: React.FC<PropsMessage> = ({ text, isSuccess, messageSuccess, messageError, isValidated }) => {
-  const message = messageSuccess && isSuccess === true ? messageSuccess : messageError;
-  const messageType = isSuccess === true ? 'Success: ' : 'Error:';
+const MessageStatus: React.FC<PropsMessage> = ({
+  text,
+  isSuccess,
+  messageSuccess,
+  messageError,
+  isValidated,
+}) => {
+  const message =
+    messageSuccess && isSuccess === true ? messageSuccess : messageError;
+  const messageType = isSuccess === true ? "Success: " : "Error:";
 
   return (
     <>
-        {isValidated === false ?
-            <P isValidated={isValidated} isSuccess={isSuccess}>
-            blank spaques in the form
-            </P>
-            :
-            <P isValidated={isValidated} isSuccess={isSuccess}>
-            {message ? message : `${messageType}${text}`}
-            </P>
-        }
+      {isValidated === false ? (
+        <P isValidated={isValidated} isSuccess={isSuccess}>
+          blank spaques in the form or the form has already been sent
+        </P>
+      ) : (
+        <P isValidated={isValidated} isSuccess={isSuccess}>
+          {message ? message : `${messageType}${text}`}
+        </P>
+      )}
     </>
   );
 };

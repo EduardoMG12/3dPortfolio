2db528d670b9a8d2123232098f10a41be4edc979
Eduardo <eduardo@pop-os.localdomain>
2023-03-15 01:32:50 -0300

    feat(Cube/3dAnimation): created object 3d

--- src/components/Who/index.tsx
+++ src/components/Who/index.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import { Container, ContentLeft, Section } from './styles';
-import Content from '@components/Content';
+import React, { Suspense } from "react";
+import { Container, ContentLeft, Section } from "./styles";
+import Content from "@components/Content";
+import Cube from "@components/Cube";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 const Who: React.FC = () => {
-    return (
-        <Container>
-            <Section>
-                <ContentLeft>{/* 3d model */}</ContentLeft>
-                <Content flex='1' textTitle='Think outside the square space' textSubtitle='What we are' textDesc='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor' childrenButton="see our projects" typeButton={"12rem"} />
-            </Section>
-        </Container >);
-}
+  return (
+    <Container>
+      <Section>
+        <ContentLeft>
+          <Canvas camera={{ position: [5, 5, 5], fov: 25 }}>
+            <Suspense fallback={null}>
+              <ambientLight intensity={0.5} />
+              <directionalLight position={[3, 2, 1]} />
+              <Cube />
+              <OrbitControls enableZoom={false} autoRotate />
+            </Suspense>
+          </Canvas>
+        </ContentLeft>
+        <Content
+          flex="1"
+          textTitle="Think outside the square space"
+          textSubtitle="What we are"
+          textDesc="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor"
+          childrenButton="see our projects"
+          typeButton={"12rem"}
+        />
+      </Section>
+    </Container>
+  );
+};
 
 export default Who;
